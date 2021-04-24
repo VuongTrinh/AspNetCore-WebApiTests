80ad6a4e1e8710c481414f38ac02f55d3ef26be7
VuongTrinh <vuongtrinh.dev@gmail.com>
2021-04-24 17:37:30 +0700

    Add Web Api Integration Test.

---
+++ src/WebApiSample/Services/StudentsService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace WebApiSample.Services
+{
+    public interface IStudentsService
+    {
+        string GetStudentNameById(int studentId);
+    }
+
+    public class StudentsService : IStudentsService
+    {
+        private readonly ILogger<StudentsService> _logger;
+        private readonly Dictionary<int, string> _studentNamesDictionary = new Dictionary<int, string>
+        {
+            {123, "Sample Name 1"},
+            {124, "Sample Name 2"},
+            {125, "Sample Name 3"},
+            {126, "Sample Name 4"},
+        };
+        public StudentsService(ILogger<StudentsService> logger)
+        {
+            _logger = logger;
+        }
+
+        public string GetStudentNameById(int studentId)
+        {
+            _studentNamesDictionary.TryGetValue(studentId, out var name);
+            return name;
+        }
+    }
+}
---
+++ src/WebApiSample/Startup.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using WebApiSample.Services;
+
+namespace WebApiSample
+{
+    public class Startup
+    {
+        // https://codeburst.io/integration-tests-for-asp-net-core-web-apis-using-mstest-f4e222a3bc8a
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyWebApp", Version = "v1" });
+            });
+
+            services.AddScoped<IStudentsService, StudentsService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyWebApp v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
